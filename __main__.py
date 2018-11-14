73e4466427cd8515ccf7bc777c36c4f9640e4ae9
christgaillard <christ.gaillard@gmail.com>
2018-11-14 16:45:25 +0100

    premier test Qt

--- __main__.py
+++ __main__.py
@@ -1,7 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import wx
+import sys
+import random
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main':
+    
+    app = QApplication(sys.argv)
+    
+    w = QWidget()
+    w.resize(250,150)
+    w.move(100,100)
+    w.setWindowTitle('simple test')
+    w.show()
+    
+    #now = QDateTime.currentDateTime()
+
+    #print(now.toString(Qt.ISODate))
+    #print(now.toString(Qt.DefaultLocaleLongDate))
+    
+    sys.exit(app.exec_())
+
 
 
 
